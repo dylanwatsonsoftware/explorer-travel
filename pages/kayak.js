3a9e90a9779d6020d5a0174b83e957916e4bb523
Dylan Watson <lotsabackscatter@gmail.com>
2018-04-08 17:21:15 +0800

    Actually go to the homepage

--- pages/kayak.js
+++ pages/kayak.js
@@ -9,11 +9,11 @@ Kayak.prototype.search = function (src) {
     return new Promise(async (resolve, reject) => {
         osmosis
             // TODO: Use https://www.kayak.com.au/s/horizon/exploreapi?airport=PER&v=1&initLoad=false
-            //           https://www.kayak.com.au/s/horizon/exploreapi?airport=PER&v=1&exactDates=true&depart=20180405&return=20180412
+            .get('https://www.kayak.com.au/')
             .get(`https://www.kayak.com.au/explore/${src}`) // Set the cookie (Maybe an easier way?)
             .get(`https://www.kayak.com.au/s/horizon/exploreapi?airport=${src}&v=1&initLoad=false`)
             .then((context, data, next) => {
-                resolve(JSON.parse(context.response.data));
+                resolve(context.response.data);
             })
             .error(function (msg) {
                 reject(msg);
