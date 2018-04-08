189cc18b6288f8ebbb30ed36c72ba9c88ee69c9d
Dylan Watson <lotsabackscatter@gmail.com>
2018-04-08 17:16:20 +0800

    Go to the home page first

--- pages/kayak.js
+++ pages/kayak.js
@@ -10,7 +10,7 @@ Kayak.prototype.search = function (src) {
         osmosis
             // TODO: Use https://www.kayak.com.au/s/horizon/exploreapi?airport=PER&v=1&initLoad=false
             //           https://www.kayak.com.au/s/horizon/exploreapi?airport=PER&v=1&exactDates=true&depart=20180405&return=20180412
-            //.get(`https://www.kayak.com.au/explore/${src}`) // Set the cookie (Maybe an easier way?)
+            .get(`https://www.kayak.com.au/explore/${src}`) // Set the cookie (Maybe an easier way?)
             .get(`https://www.kayak.com.au/s/horizon/exploreapi?airport=${src}&v=1&initLoad=false`)
             .then((context, data, next) => {
                 resolve(JSON.parse(context.response.data));
